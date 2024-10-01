1d275768ab5a2e8ca0d9957082b8c81566fd4649
Nahuel Amado <amado.nahuel@outlook.com>
2024-10-01 17:20:53 -0300

    Actualizaciones de código de los cursos

---
+++ jsConDalto/junior_a_mid_lvl/eventos/addEventListenerVsEventHandlers/code.js
@@ -0,0 +1,31 @@
+const button = document.querySelector(".button");
+const button2 = document.querySelector(".button2");
+const button3 = document.querySelector(".button3");
+const button4 = document.querySelector(".button4");
+
+button.onclick = ()=>{//event handlers fuera de uso, dan problemas
+    alert("hola XD");
+}
+
+button2.addEventListener("click", saludar);//añadimos escucha de evento
+
+// const saludar = ()=>{   NO FUNCIONA funciones flecha ni parametros---SOLO SINTAXIS DE FUnCTION COMUNES
+//     alert("Hola manola");
+// }
+
+function saludar(){//sin parametros
+    alert("holis");
+}
+
+//////////_____MEJOR______///////////
+
+button3.addEventListener("click", ()=>{//correcto y con parametros
+    alert("Hola de forma correcta!");
+});
+
+button4.addEventListener("click",saludarUnaVez);
+
+function saludarUnaVez(){
+    alert("Buenos Días");
+    button4.removeEventListener("click",saludarUnaVez)///borra el escuchador
+}
